6be38779f2ed65eafb4d1f926e1eac9948a93541
scarter31191 <scarter31191@gmaile.com>
2021-12-31 11:49:54 -0500

    fixed college and student has_and_belongs_to_many relationship

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,3 +1,3 @@
 class College < ApplicationRecord
-    has_and_belongs_to_many :colleges
+    has_and_belongs_to_many :students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,3 @@
 class Student < ApplicationRecord
-    has_and_belongs_to_many :students
+    has_and_belongs_to_many :colleges
 end
