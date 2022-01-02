e40320042a329a7c6cc1b3ece840e3dfe923531e
scarter31191 <scarter31191@gmaile.com>
2022-01-02 11:42:05 -0500

    created college and exam relationship

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,3 +1,4 @@
 class College < ApplicationRecord
+    has_many :exams
     has_and_belongs_to_many :students
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_153603) do
+ActiveRecord::Schema.define(version: 2022_01_02_162637) do
 
   create_table "colleges", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_12_31_153603) do
     t.index ["student_id", "college_id"], name: "index_colleges_students_on_student_id_and_college_id"
   end
 
+  create_table "exams", force: :cascade do |t|
+    t.string "name"
+    t.integer "college_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["college_id"], name: "index_exams_on_college_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_12_31_153603) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "exams", "colleges"
 end
